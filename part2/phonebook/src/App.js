fd4477e78cf4fec8f89bb92b1c1ea02dbc9d34a7
Jack <JJB9922@protonmail.com>
2023-05-15 15:51:22 +0100

    p2c

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import FilteredDisplay from './components/FilteredDisplay'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
   //State handlers
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchVal, setSearchVal] = useState('')
+
+  //Fetch data from database
+  const getHook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          setPersons(response.data)
+        },)
+  }
+
+  useEffect(getHook, [])
+  console.log("There are ", persons.length, " people")
 
   //Event handlers
   const handleNameChange = (event) =>{
