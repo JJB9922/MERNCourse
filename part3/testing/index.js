53eb705303a32a19390019195c1490866db5437e
Jack <JJB9922@protonmail.com>
2023-05-29 11:33:15 +0100

    Updating before exercise 3 - will be in a new repo under MERNPhonebook.

--- part3/testing/index.js
+++ part3/testing/index.js
@@ -1,5 +1,7 @@
-const http = require('http')
-//Imports Node's built-in web server module
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -19,13 +21,44 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if(note){
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id != id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    
+    const note = request.body
+    note.id = maxId + 1
+
+    notes = notes.concat(note)
+
+
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
-app.listen(PORT)
-//Binds http server to the port 3001
-
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
